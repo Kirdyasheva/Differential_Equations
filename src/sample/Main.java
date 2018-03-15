efcec983c6b983b99f28f964eb83731aeb7cd712
kirdyasheva <kirdyasheva98@gmail.com>
2018-03-15 16:05:44 +0300

    plotting are added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -29,22 +29,31 @@ public class Main extends Application {
         double x0 = 0;
         double y0 = 3;
         double x = 5.5;
-        double n = 0.01; //the size of the step
+        double n = 0.1; //the size of the step
 
         double currentX = x0;
         double currentY = y0;
 
+        ObservableList<XYChart.Data> data = FXCollections.observableArrayList();
+
         while (currentX != x + n) { //the euler method and the chart plotting
+            data.add(new XYChart.Data(currentX, currentY)); //adding new points for the plot
+
             currentY += n * func(currentX, currentY); //euler method calculation
             currentX += n; //euler method calculation
         }
 
-        primaryStage.setScene(new Scene(root, 1200, 600));
+        mainChart.setData(data);
+
+        Scene scene = new Scene(chart, 600, 600);
+        chart.getData().add(mainChart);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public double func(double x, double y) {
-        return x * y * y + 3 * x * y;
+        return (x * y * y) + (3 * x * y);
     }
 
     public static void main(String[] args) {
