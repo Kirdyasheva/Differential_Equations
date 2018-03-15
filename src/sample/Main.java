93203598c90d0caaf82f3ee8f5dd730285b5827b
kirdyasheva <kirdyasheva98@gmail.com>
2018-03-15 19:10:48 +0300

    problem with drawing are solved

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,35 +15,37 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Chart");
 
-        NumberAxis xAxis = new NumberAxis();
-        NumberAxis yAxis = new NumberAxis();
+        NumberAxis xAxis = new NumberAxis(0,5,1);
+        NumberAxis yAxis = new NumberAxis(-10,1000,1);
 
         LineChart<Number, Number> chart = new LineChart<Number, Number>(xAxis, yAxis);
         chart.setTitle("Euler method of approximation");
         XYChart.Series mainChart = new XYChart.Series();
-        mainChart.setName("My chart");
+        mainChart.setName("xy^2 + 3xy");
 
         double x0 = 0.0;
         double y0 = 3.0;
         double x = 5.5;
-        double n = 0.1; //the size of the step
+        double n = 0.01; //the size of the step
 
         double currentX = x0;
         double currentY = y0;
 
-        ObservableList<XYChart.Data> data = FXCollections.observableArrayList();
+        //ObservableList<XYChart.Data> data = FXCollections.observableArrayList();
 
         while (currentX <= x + n) { //the euler method and the chart plotting
-            data.add(new XYChart.Data(currentX, currentY)); //adding new points for the plot
+            //data.add(new XYChart.Data(currentX, currentY)); //adding new points for the plot
+
+            mainChart.getData().add(new XYChart.Data(currentX, currentY));
 
             currentY += n * func(currentX, currentY); //euler method calculation
             currentX += n; //euler method calculation
         }
 
-        mainChart.setData(data);
+        //mainChart.setData(data);
 
         Scene scene = new Scene(chart, 600, 600);
         chart.getData().add(mainChart);
