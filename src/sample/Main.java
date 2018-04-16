4d4ad9bc9a15c4bf55afea1f34e5ad8fb29e88ea
kirdyasheva <kirdyasheva98@gmail.com>
2018-04-16 10:41:50 +0300

    new method was added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,35 +1,34 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
-public class Main extends Application {
 
+public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Chart");
 
-        NumberAxis xAxis = new NumberAxis(0,5,1);
-        NumberAxis yAxis = new NumberAxis(-10,1000,1);
+        NumberAxis xAxis = new NumberAxis(0, 5, 1);
+        NumberAxis yAxis = new NumberAxis(-10, 1000, 1);
 
         LineChart<Number, Number> chart = new LineChart<Number, Number>(xAxis, yAxis);
         chart.setTitle("xy^2 + 3xy");
         XYChart.Series series1 = new XYChart.Series();
+        XYChart.Series series2 = new XYChart.Series();
+
         series1.setName("Euler method");
+        series2.setName("Improved Euler method");
 
         double x0 = 0.0;
         double y0 = 3.0;
         double x = 5.5;
-        double n = 0.001; //the size of the step
+        double n = 0.0; //the size of the step
 
         double currentX = x0;
         double currentY = y0;
@@ -47,11 +46,23 @@ public class Main extends Application {
 
         //series1.setData(data);
 
+        currentX = x0;
+        currentY = y0;
+
+        while (currentX <= x + n) {
+            series2.getData().add(new XYChart.Data(currentX, currentY)); //adding data to the chart
+
+            currentY += 0.5 * n * (func(currentX, currentY) + func(currentX + n, currentY + n * func(currentX, currentY)));
+            currentX += n; //improved Euler method calculation
+        }
+
         Scene scene = new Scene(chart, 600, 600);
         chart.getData().add(series1);
+        chart.getData().add(series2);
 
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
     public double func(double x, double y) {
