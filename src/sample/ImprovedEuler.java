75dc616b150d3a4d1c10498398d5a0d8b389df9b
kirdyasheva <kirdyasheva98@gmail.com>
2018-04-20 23:39:35 +0300

    the euler method was corrected

--- src/sample/ImprovedEuler.java
+++ src/sample/ImprovedEuler.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.scene.chart.XYChart;
+
 public class ImprovedEuler {
+    public static XYChart.Series Eulers(double x0, double y0, double x, double n) {
+        double currentX = x0;
+        double currentY = y0;
+        XYChart.Series series = new XYChart.Series();
+
+        while (currentX <= x + n) { //the euler method and the chart plotting
+
+            series.getData().add(new XYChart.Data(currentX, currentY));
+
+            currentY += n * Function.func(currentX + n * 0.5, currentY + n * 0.5 * Function.func(currentX, currentY));
+            currentX += n; //euler method calculation
+        }
+        return series;
+    }
 }
